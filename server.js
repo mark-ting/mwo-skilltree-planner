bc9b11e5875b4cf8b9255f47250017370619395a
Mark Ting <mat3049@gmx.com>
2017-05-29 12:39:05 +0800

    Moved index and resources to root to support GitHub site hosting

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path')
 const app = express()
 let port = 3000
 
-const src = path.join(__dirname, '/public')
+const src = path.join(__dirname, '/')
 app.use(express.static(src))
 
 app.listen(port, () => {
